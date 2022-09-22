8361bd06f857fd24aadd51eb368aedfdb55c3f85
Juber123github <khatikjuber786@gmail.com>
2022-09-22 21:45:49 +0530

    arraylist

---
+++ ProjectDemo/src/com/velocity/collection/Demo.java
@@ -0,0 +1,19 @@
+package com.velocity.collection;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = Arrays.asList(100,200,300,400);
+		
+		Iterator<Integer> itr = list.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+}
